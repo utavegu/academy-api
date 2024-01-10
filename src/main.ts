d88ba486495e5eb17a82c5731379eb3980873a92
Utavegu <utavegu@list.ru>
2024-01-10 18:52:16 +0500

    feat: add global extended exception filter

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 import { NestExpressApplication } from '@nestjs/platform-express/interfaces/nest-express-application.interface';
 import { serverPort } from './config/variables';
+import { ExtendedException } from './common/extended-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix('api').useGlobalFilters(new ExtendedException());
 
   await app.listen(serverPort, () =>
     console.log(`Сервер успешно стартанул на ${serverPort} порту!`),
